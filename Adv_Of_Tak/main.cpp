3c8298da385b781e0471589a1f5a4a5483384db3
sfml_game <jht960526@naver.com>
2021-06-24 21:29:43 +0900

    first sfml
    처음 커밋입니다!!

--- Adv_Of_Tak/main.cpp
+++ Adv_Of_Tak/main.cpp
@@ -1,6 +1,34 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500,500),"WINDOW");
+	RenderWindow window(sf::VideoMode(500,500),"WINDOW");
+
+	//사각형 오브젝트
+	//사이즈는 100,f x 100,f
+	//색상은 Green
+	//위치는 100.f,100.f
+	RectangleShape rs;
+
+	//Vector2f = float 자료형 두개짜리
+	rs.setFillColor(Color::Green);
+	rs.setSize(Vector2f(100.f,100.f)); // 2.0은 double형
+	rs.setPosition(Vector2f(250.f,250.f));
+	// 오브젝트의 중심을 set하는 함수
+	rs.setOrigin(rs.getSize() / 2.f);
+
+	while (window.isOpen())
+	{
+		window.clear();
+
+		rs.setPosition(rs.getPosition().x + 0.05f,rs.getPosition().y);
+
+		//rs를 윈도우에 그려라!
+		window.draw(rs);
+
+		//윈도우에 그린 결과물을 출력하라
+		window.display();
+	}
 }
