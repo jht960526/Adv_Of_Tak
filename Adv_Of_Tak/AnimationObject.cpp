111a35c38dc0954367fdcdbc783d21fd8848c185
sfml_game <jht960526@naver.com>
2021-07-01 18:49:34 +0900

    naruto update
    나루토 추가

--- Adv_Of_Tak/AnimationObject.cpp
+++ Adv_Of_Tak/AnimationObject.cpp
@@ -16,9 +16,9 @@ void AnimationObject::Init()
 	Texture* tx = nullptr;
 
 	char filePath[50];
-	for(int i = 32; i < 36; ++i)
+	for(int i = 2; i < 7; ++i)
 	{
-		sprintf(filePath, "Textures/cookie0020x2/cookie0020x2_00%d.png",i);
+		sprintf(filePath, "Textures/Naruto/naruto_idle_00%d.png",i);
 		tx = new Texture;
 		tx->loadFromFile(filePath);
 		this->vAnimation.push_back(tx);
